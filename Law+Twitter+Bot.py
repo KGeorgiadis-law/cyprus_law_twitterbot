8a37c7d1ffb4af3235e02c00ade5b6915f171165
Konstantinos Georgiadis <kostas.georgiadis363@gmail.com>
2017-05-24 15:46:17 +0100

    Check to shorten tweets

--- Law+Twitter+Bot.py
+++ Law+Twitter+Bot.py
@@ -72,7 +72,7 @@ while True:
         last_ETag = ETag_file.read()
     except FileNotFoundError:
         ETag_file = open("last_ETag.txt", "w")
-        last_ETag = None
+        last_ETag = ''
     ETag_file.close()
     print(current_ETag+" "+last_ETag)
 
@@ -147,12 +147,21 @@ while True:
                 link = link_prefix + ann.get('href')
                 try:
                     tweet_text = "["+str(counter)+"/"+no_announcements+"]: "+text+" "+link
+
+                    # check if the length exceeds standard tweet length
+                    # explanation: links are shortened by twitter to always be 23 characters.
+                    # therefore, we can substract the length fo the link and add 23 to find the real
+                    # length of the tweet.
+                    # If it is more than 140 characters, the text is shortened to its first 105 characters
+                    #  (this is considering that link = 23 chars and counter/no announcements = 9 chars)
+                    #  (140 - (23 + 9) = 108)
+                    if len(tweet_text) - len(link) + 23 > 140:
+                        print("Length larger than 140 characters so shortening length to first 105 chars")
+                        tweet_text = "["+str(counter)+"/"+no_announcements+"]: "+text[0:105]+"... "+link
                     api.update_status(tweet_text)
                     print("Posting...")
                     print(tweet_text)
-                except: #only post the first 75 characters of the case if the script gets a twitter error
-                    #tweet_text = "["+str(counter)+"/"+no_announcements+"]: "+text[0:75]+" "+link 
-                    #api.update_status(tweet_text)
+                except: 
                     print("Error encountered! Skipping posting of this tweet...")
                     print("For reference, tweet length was {} and its text was {}".format(len(tweet_text), tweet_text))
                 sleep(5)
