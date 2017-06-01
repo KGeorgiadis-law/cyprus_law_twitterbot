20f5e1c7048e3485044678175542dfd3e56b0de6
Konstantinos Georgiadis <kostas.georgiadis363@gmail.com>
2017-06-01 20:44:41 +0300

    Check for duplicate tweets added

--- Law+Twitter+Bot.py
+++ Law+Twitter+Bot.py
@@ -2,29 +2,24 @@
 
 # coding: utf-8
 
-# In[18]:
-
-#!Py3.5.2
+# !Py3.5.2
 #  CyprusLawTwitterBot_ver2.py
 #  Mostly a learning exercise.
 #  A bot to:
-##  (a) check CyLaw once an hour on weekdays for an update
-##  (b) if it finds an update, take the title and a link to the document
-##  (c) post these on twitter as an announcement.
+#  (a) check CyLaw once an hour on weekdays for an update
+#  (b) if it finds an update, take the title and a link to the document
+#  (c) post these on twitter as an announcement.
 
-## How does it do these things?
-### (a) is done using an HTTP request to see whether something has changed
-### (b) is done using BeautifulSoup4 to find the latest link added to the page
-### (c) is done using the twitter API
+# How does it do these things?
+# (a) is done using an HTTP request to see whether something has changed
+# (b) is done using BeautifulSoup4 to find the latest link added to the page
+# (c) is done using the twitter API
 
-## Where will the bot run?
-### The plan is to have the tweeterbot run on my Amazon EC2 instance -
-### but we'll see how that goes.
+# Where will the bot run?
+# The plan is to have the tweeterbot run on my Amazon EC2 instance -
+# but we'll see how that goes.
 
-
-# In[94]:
-
-#dependencies
+# dependencies
 
 import tweepy
 
@@ -38,60 +33,52 @@ from urllib.request import urlopen
 
 from time import sleep, gmtime, strftime
 
-
-
-
-# In[29]:
-
 while True:
 
     url = "http://www.cylaw.org/updates.html"
 
-    print("Starting...")
+    print("Starting... ", strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-    # In[56]:
+    
 
     # method 1: only request the headers to avoid making a whole GET request every time 
     response = requests.head(url)
     headers = response.headers
 
-    print(headers['ETag'])
-    current_ETag = headers['ETag'] 
-    # for more info on ETags and why they're used here, see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag
+    current_ETag = headers['ETag']
+    # for more info on ETags and why they're used here,
+    # see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag
 
-    # In[71]:
     # log the time of check
-    log_file = open("log.txt", "a")
-    log = strftime("%Y-%m-%d %H:%M:%S", gmtime())+" : "+current_ETag+"\n"
-    log_file.write(log)
-    log_file.close()
+    with open("log.txt", "a") as log_file:
+        log = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " : " + current_ETag + "\n"
+        log_file.write(log)
 
-    #next step: try to read last saved ETag - if file does not exist, create it
+    # next step: try to read last saved ETag - if file does not exist, create it
     try:
         ETag_file = open("last_ETag.txt", "r")
         last_ETag = ETag_file.read()
     except FileNotFoundError:
+        print("FileNotFoundError")
         ETag_file = open("last_ETag.txt", "w")
-        last_ETag = None
+        last_ETag = ''
     ETag_file.close()
-    print(current_ETag+" "+last_ETag)
+    print(current_ETag + " " + last_ETag)
 
-    if current_ETag != last_ETag: #change this to != for production environment
-        
+    if current_ETag == last_ETag:  # change this to != for production environment
+
         print("change detected!")
-        #update the ETag file with the new ETag
+        # update the ETag file with the new ETag
         ETag_file = open("last_ETag.txt", "w")
         ETag_file.write(current_ETag)
-        
+        ETag_file.close()
+
         # change of ETag means that the page has been updated. Open the site properly (with a GET request).
         # using URLopen
         html = urlopen(url, timeout=30)
 
-        # In[72]:
-
-        #create a beautiful soup object that we can then search through
+        # create a beautiful soup object that we can then search through
         soup = BeautifulSoup(html, "html.parser")
-
 
         # Method::
         # 1. Find div id='inner-content' x
@@ -100,22 +87,17 @@ while True:
         # 4. get the div after that tag x
         # 5. get all `<a>` tags inside that tag x
 
-        # In[74]:
+        container = soup.find(id="inner-content")  # <div> with all contents
+        title_card = container.h3  # the title card for the central div
 
-        container = soup.find(id="inner-content") #<div> with all contents
-        title_card = container.h3 #the title card for the central div
+        # the first announcement after the title.
+        # it seems that this is going to be the most recent one
+        latest_announcement = title_card.find_next_sibling("h3")
+        date = latest_announcement.contents[1]
+        # second in the list of contents for the <h3> tag is consistently the date
 
-        #the first announcement after the title. 
-        #it seems that this is going to be the most recent one
-        latest_announcement = title_card.find_next_sibling("h3") 
-        date = latest_announcement.contents[1] #second in the list of contents for the <h3> tag is consistently the date
-
-
-        # In[101]:
-
-        # all new decisions are contained within <a> tags
-        # in the div after <h3>
-        announcements = latest_announcement.find_next_sibling("div").findAll("a") 
+        # all new decisions are contained within <a> tags in the div after <h3>
+        announcements = latest_announcement.find_next_sibling("div").findAll("a")
 
         link_prefix = "http://www.cylaw.org"
 
@@ -125,43 +107,66 @@ while True:
 
         if len(announcements) > 0:
 
-            previous_tweets_file = open("previous_tweets.txt", "a")
-            
-            #start drafting a tweet
-            tweet_list = []
+            with open("previous_tweets.txt", "r", encoding='utf-8') as previous_tweets_file:
+                previous_tweets_list = previous_tweets_file.readlines()
+
+            previous_tweets_file = open("previous_tweets.txt", "a", encoding='utf-8')
+
+            # start drafting a tweet
+
             no_announcements = str(len(announcements))
-            starting_tweet = (date+": Δημοσίευση "+no_announcements+" Νέων αποφάσεων στο http://www.cylaw.org/updates.html : ...")
-            try:
-                api.update_status(starting_tweet)
-                print(starting_tweet)
-            except: #only post the first 75 characters of the case if the script gets a twitter error
-                #tweet_text = "["+str(counter)+"/"+no_announcements+"]: "+text[0:75]+" "+link 
-                #api.update_status(tweet_text)
-                print("Error encountered! Skipping posting of this tweet...")
-                print("For reference, tweet length was {} and its text was {}".format(len(starting_tweet), starting_tweet))
-            
+            starting_tweet = (
+                date + ": Δημοσίευση " + no_announcements +
+                " Νέων αποφάσεων στο http://www.cylaw.org/updates.html : ...")
+            if starting_tweet + "\n" not in previous_tweets_list:
+                previous_tweets_file.write(starting_tweet+"\n")
+                try:
+                    api.update_status(starting_tweet)
+                    print(starting_tweet)
+                except:  # only post the first 75 characters of the case if the script gets a twitter error
+                    # tweet_text = "["+str(counter)+"/"+no_announcements+"]: "+text[0:75]+" "+link
+                    # api.update_status(tweet_text)
+                    print("Error encountered! Skipping posting of this tweet...")
+                    print("For reference, tweet length was {} and its text was {}".format(len(starting_tweet),
+                                                                                          starting_tweet))
+            else:
+                print("Duplicate tweet!\n{}".format(starting_tweet))
+
+
             counter = 1
 
-            for ann in announcements: #compile and send tweets
+            for ann in announcements:  # compile and send tweets
                 text = ann.text
                 link = link_prefix + ann.get('href')
-                try:
-                    tweet_text = "["+str(counter)+"/"+no_announcements+"]: "+text+" "+link
-                    api.update_status(tweet_text)
-                    print("Posting...")
-                    print(tweet_text)
-                except: #only post the first 75 characters of the case if the script gets a twitter error
-                    #tweet_text = "["+str(counter)+"/"+no_announcements+"]: "+text[0:75]+" "+link 
-                    #api.update_status(tweet_text)
-                    print("Error encountered! Skipping posting of this tweet...")
-                    print("For reference, tweet length was {} and its text was {}".format(len(tweet_text), tweet_text))
+                tweet_text = "[" + str(counter) + "/" + no_announcements + "]: " + text + " " + link
+                if tweet_text + "\n" not in previous_tweets_list:
+                    previous_tweets_file.write(tweet_text+"\n")
+                    try:
+                        # check if the length exceeds standard tweet length
+                        # explanation: links are shortened by twitter to always be 23 characters.
+                        # therefore, we can substract the length fo the link and add 23 to find the real
+                        # length of the tweet.
+                        # If it is more than 140 characters, the text is shortened to its first 105 characters
+                        #  (this is considering that link = 23 chars and counter/no announcements = 9 chars)
+                        #  (140 - (23 + 9) = 108)
+                        if len(tweet_text) - len(link) + 23 > 140:
+                            print("Length larger than 140 characters so shortening length to first 105 chars")
+                            tweet_text = "[" + str(counter) + "/" + no_announcements + "]: " + text[0:105] + "... " + link
+                        api.update_status(tweet_text)
+                        print("Posting...")
+                        print(tweet_text)
+                        # previous_tweets_file.write()
+                    except:
+                        print("Error encountered! Skipping posting of this tweet...")
+                        print("For reference, tweet length was {} and its text was {}".format(len(tweet_text), tweet_text))
+                else:
+                    print("Duplicate tweet!")
                 sleep(5)
                 counter += 1
             previous_tweets_file.close()
-        
+
         print("Finished! Sleeping now... \n\n")
     else:
         print("No changes since last time! Sleeping now...\n\n")
 
-    sleep(600) #do this every 600 seconds (5 mins)
-
+    sleep(600)  # do this every 600 seconds (5 mins)
