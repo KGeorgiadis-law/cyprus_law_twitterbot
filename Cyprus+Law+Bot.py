ecc5be91dbb5d999b0e13688f70f01df01fbfd0b
Konstantinos Georgiadis <kostas.georgiadis363@gmail.com>
2018-12-08 15:33:40 +0000

    heroku

--- Cyprus+Law+Bot.py
+++ Cyprus+Law+Bot.py
@@ -34,7 +34,7 @@ from credentials import * # log in information
 
 # initialise twitter API
 
-def cyprus_law_bot():
+def CyprusLawBot():
 
     # log in
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -87,11 +87,12 @@ def cyprus_law_bot():
 
         if current_ETag == last_ETag:
             print("No changes since last time! Sleeping now...\n\n")
-            return "No changes detected!"
+            # exit program
+            return ""
         
         print("change detected!")
 
-            # update the ETag file with the new ETag
+        # update the ETag file with the new ETag
         with open("last_ETag.txt", "w") as ETag_file:
             ETag_file.write(current_ETag)
 
@@ -100,89 +101,91 @@ def cyprus_law_bot():
         print("Requesting full HTML...")
         html = urlopen(url, timeout=30)
 
-            # create a beautiful soup object from the HTML received
-            print("Creating beautiful soup object...")
-            soup = BeautifulSoup(html, "html.parser")
+        # create a beautiful soup object from the HTML received
+        print("Creating beautiful soup object...")
+        soup = BeautifulSoup(html, "html.parser")
 
 	    # Get the date of the latest update on cylaw.org/updates and
 	    # get links to the latest cases.
 
-            # Method to parse (analyse) HTML received
-            # 1. Find `<div id='inner-content'>`
-            # 2. Find 2nd `<h3>` tag (1st `<h3>` tag is the title card)
-            # 3. Collect the date from that `<h3>` tag
-            # 4. get the div after that tag
-            # 5. get all `<a>` tags inside that tag
+        # Method to parse (analyse) HTML received
+        # 1. Find `<div id='inner-content'>`
+        # 2. Find 2nd `<h3>` tag (1st `<h3>` tag is the title card)
+        # 3. Collect the date from that `<h3>` tag
+        # 4. get the div after that tag
+        # 5. get all `<a>` tags inside that tag
 
-            print("Parsing HTML...")
-            container = soup.find(id="inner-content")  # <div> with all contents
-            title_card = container.h3  # the title card for the central <div>
+        print("Parsing HTML...")
 
-            # the first announcement after the title card is the most recent one
-            latest_announcement = title_card.find_next_sibling("h3")
+        # step 1
+        container = soup.find(id="inner-content")  # <div> with all contents
+        
+        # step 2
+        title_card = container.h3  # the title card for the central <div>
+        # the first announcement after the title card is the most recent one
+        latest_announcement = title_card.find_next_sibling("h3")
 
 	    # the date is consistently second in the list of contents for the latest announcement
-            latest_announcement_date = latest_announcement.contents[1]
-            # all new decisions are contained within <a> tags in the div after <h3>
-            judgements_links = latest_announcement.find_next_sibling("div").findAll("a")
-            link_prefix = "http://www.cylaw.org"
+        latest_announcement_date = latest_announcement.contents[1]
+        # all new decisions are contained within <a> tags in the div after <h3>
+        judgements_links = latest_announcement.find_next_sibling("div").findAll("a")
+        link_prefix = "http://www.cylaw.org"
 
-            if len(judgements_links) == 0:
+        if len(judgements_links) == 0:
+            print("No judgements found! That's strange...")
+    		##TODO: set hard limit on amount of judgements that can be posted.
+            return ""
 
-                print("No judgements found! That's strange...")
-		##TODO: set hard limit on amount of judgements that can be posted.
+        # start drafting opening tweet
+        print("Judgements found! Starting drafting process...")
+        new_judgements_count = str(len(judgements_links))
+        prefix = "νέας απόφασης" if new_judgements_count == 1 else "νέων αποφάσεων"
 
-            else:
-
-                # start drafting opening tweet
-                print("Judgements found! Starting drafting process...")
-                new_judgements_count = str(len(judgements_links))
-                prefix = "νέας απόφασης" if new_judgements_count == 1 else "νέων αποφάσεων"
-
-		# draft text for starting tweet
-                starting_tweet = "{}: Δημοσίευση {} {} Ανωτάτου Κύπρου {}".format(latest_announcement_date, new_judgements_count, prefix, url)
+        # draft text for starting tweet
+        starting_tweet = "{}: Δημοσίευση {} {} Ανωτάτου Κύπρου {}".format(latest_announcement_date, new_judgements_count, prefix, url)
 
 		# count tweets posted. Tweet #0 is the starting tweet
-                tweet_counter = 0
+        tweet_counter = 0
 
 		# post starter tweet
-                print("Posting Starter tweet...")
-                first_tweet = post_tweet(tweet_counter, starting_tweet, api, tweet_counter)
+        print("Posting Starter tweet...")
+        first_tweet = post_tweet(tweet_counter, starting_tweet, api, tweet_counter)
 
-                if first_tweet != False or first_tweet == None:
-                    print("Success: tweet id: {}".format(first_tweet.id_str))
+        if first_tweet != False or first_tweet == None:
 
-                    # draft and send tweets
-                    for judgement in judgements_links:
+            print("Success: tweet id: {}".format(first_tweet.id_str))
 
-                        tweet_counter += 1
+            # draft and send tweets
+            for judgement in judgements_links:
 
-                        judgement_title = judgement.text
-                        print(judgement_title)
+                tweet_counter += 1
 
-                        # check if the length exceeds standard tweet length
-                        # explanation of 108/105 char limit:
-                        # links (urls) are automatically shortened by twitter to 23 characters;
-                        # therefore, we can substract the length fo the link and add 23 to find the real
-                        # length of the tweet.
-                        # If it is more than 140 characters, the text is shortened to its first 105 characters
-                        #  (this is considering that link = 23 chars and counter/no announcements = up to 9 chars, since judgement number may be a 2-digit number)
-                        #  (140 - (23 + 7) = 108)
-                        # Finally, if a title is over 108 characters, we reduce it to 105 and add 3 fullstops
+                judgement_title = judgement.text
+                print(judgement_title)
 
-                        if len(judgement_title) > 105:
+                # check if the length exceeds standard tweet length
+                # explanation of 108/105 char limit:
+                # links (urls) are automatically shortened by twitter to 23 characters;
+                # therefore, we can substract the length fo the link and add 23 to find the real
+                # length of the tweet.
+                # If it is more than 140 characters, the text is shortened to its first 105 characters
+                #  (this is considering that link = 23 chars and counter/no announcements = up to 9 chars, since judgement number may be a 2-digit number)
+                #  (140 - (23 + 7) = 108)
+                # Finally, if a title is over 108 characters, we reduce it to 105 and add 3 fullstops
 
-                            print("Length larger than 140 characters so shortening length to first 105 chars")
-                            judgement_title = judgement.text[:105]+"..."
+                if len(judgement_title) > 105:
 
-                        judgement_url = link_prefix + judgement.get('href')
-                        tweet_text = "[{}/{}] {} {}".format(str(tweet_counter), len(judgements_links), judgement_title, judgement_url)
-                        tweet = post_tweet(tweet_counter, tweet_text, api, first_tweet)
+                    print("Length larger than 140 characters so shortening length to first 105 chars")
+                    judgement_title = judgement.text[:105]+"..."
 
-                        if tweet != False or tweet == None:
-                            print("Success: tweet id: {}".format(tweet.id_str))
+                judgement_url = link_prefix + judgement.get('href')
+                tweet_text = "[{}/{}] {} {}".format(str(tweet_counter), len(judgements_links), judgement_title, judgement_url)
+                tweet = post_tweet(tweet_counter, tweet_text, api, first_tweet)
 
-                        sleep(5) # wait five seconds before posting a new tweet
+                if tweet != False or tweet == None:
+                    print("Success: tweet id: {}".format(tweet.id_str))
+
+                sleep(5) # wait five seconds before posting a new tweet
 
             print("Finished! Sleeping now... \n\n")
 
