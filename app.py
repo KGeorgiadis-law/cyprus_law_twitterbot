14fffd27dcdcafb1417ee8831508dd3c9cdc60b5
Konstantinos Georgiadis <kostas.georgiadis363@gmail.com>
2019-01-03 19:12:35 +0000

    database effort

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from flask import request
 from CyprusLawBot import cyprusLawBot
+from os import getenv
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+
+  def __repr__(self):
+      return '<User %r>' % self.username
+
+class Etags(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  etag = db.Column(db.String(80))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
