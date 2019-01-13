5ccca7b62b623244993d9e2caa804c46d382c6ae
Konstantinos Georgiadis <kostas.georgiadis363@gmail.com>
2019-01-13 12:56:23 +0000

    set track_modifications to false

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
